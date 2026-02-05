7a492c895f9dc9b176f347eaec233f749b65aa21
HappyTanuki <rlaalstn120@icloud.com>
2026-02-05 16:12:39 +0900

    라이브러리 로더 예제 수정 & 설정파일 복사 구조 포함 & cmakelists 정리

--- test/LibraryLoadCheck.cc
+++ test/LibraryLoadCheck.cc
@@ -1,26 +1,43 @@
-#include "encryption/block_cipher/mode/aliases.h"
-#include <array>
 #include <openssl/core.h>
 #include <openssl/crypto.h>
-#include <openssl/ssl.h>
 #include <openssl/evp.h>
+#include <openssl/ssl.h>
+
+#include <array>
+#include <cstring>
+#include <iostream>
+
+#include "encryption/block_cipher/mode/aliases.h"
 
 #define ALGORITHM bedrock::cipher::AES_ECB
 
 int main() {
   std::array<std::uint8_t, 16> buffer = {};
-  std::array<std::uint8_t, 16> key = {};
+  std::array<std::uint8_t, 32> key = {0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA,
+                                      0xAA, 0xAA, 0xAA, 0xAA, 0xAA, 0xAA,
+                                      0xAA, 0xAA, 0xAA, 0xAA};
+  std::array<std::uint8_t, 16> target = {};
 
-  EVP_MD *md = EVP_MD_fetch(NULL, "SHA2-256", NULL);
+  ::EVP_MD* md = ::EVP_MD_fetch(NULL, "SHA2-256", NULL);
 
   ALGORITHM cipher(key);
   cipher << bedrock::cipher::op_mode::CipherMode::Encrypt;
 
-  while (true) {
+  std::cout << "Program is now running..." << std::endl;
+
+  std::size_t i = 0;
+  while (++i) {
     cipher.Process(buffer, buffer);
+
+    if (buffer == target) {
+      std::cout << "i: " << i << std::endl;
+      break;
+    }
+
+    buffer[i % 16] ^= static_cast<uint8_t>(i);
   }
 
-  EVP_MD_free(md);
+  ::EVP_MD_free(md);
 
   return 0;
 }
